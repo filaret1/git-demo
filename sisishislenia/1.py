a4a2858c95f62d08a269cf2a820c4ec1752797b8
Andrei Hovar <andreigovor77@gmail.com>
2022-02-18 00:12:36 +0300

    add unittest

--- sisishislenia/1.py
+++ sisishislenia/1.py
@@ -2,3 +2,11 @@ n = int(input())
 print(bin(n)[2:])
 print(oct(n)[2:])
 print(hex(n)[2:].upper())
+
+def test_substring(full_string, substring):
+    assert substring in full_string, \
+    f"expected '{substring}' to be substring of '{full_string}'"
+
+def test_substring1(full_string, substring):
+    if substring in full_string:
+        print(f"expected '{substring}' to be substring of '{full_string}'")
